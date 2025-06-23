94992b60d9753fae85fa59525a589789eb398dc5
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-23 13:39:20 -0400

    COmpleting challenge 10-leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (Number.isInteger(year)) {
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        if (year % 400 === 0) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
