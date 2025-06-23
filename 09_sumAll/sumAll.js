68d4072a3b71c629858110834f9abf629df498bf
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-23 13:33:24 -0400

    Completing challenge 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (intOne, intTwo) {
+  if (
+    Number.isInteger(intOne) &&
+    Number.isInteger(intTwo) &&
+    intOne > 0 &&
+    intTwo > 0
+  ) {
+    let sum = 0;
+    for (let i = Math.min(intOne, intTwo); i <= Math.max(intOne, intTwo); i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
