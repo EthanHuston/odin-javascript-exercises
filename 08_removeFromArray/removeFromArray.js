6d1abf5233fd5bbfd065eecfb334acb6766edd57
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-23 13:25:52 -0400

    COmpleting challenge 08-removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let arr = arguments[0];
+
+  for (let i = 1; i < arguments.length; i++) {
+    arr = arr.filter((element) => element !== arguments[i]);
+  }
+  return arr;
 };
 
 // Do not edit below this line
