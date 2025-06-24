42693ba6aa0f5208737096db9e19762e9e9e56bd
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-24 15:52:47 -0400

    Completing challenge 14-fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  val = Number(num);
+  if (val < 0) {
+    return "OOPS";
+  } else if (val === 0) {
+    return 0;
+  } else if (val === 1 || val === 2) {
+    return 1;
+  }
 
+  return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
