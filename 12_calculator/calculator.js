b1768ef05409dcef36c53131c8ad5bd858fc0b75
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-24 15:33:36 -0400

    Completing challenge 12-calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((prev, value) => {
+    return prev + value;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prev, value) => {
+    return prev * value;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
