4cb774c87a62c205784c74f1bf08e1970feb343c
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-24 16:06:37 -0400

    Completing challenge 16-findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let oldest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (getAge(oldest) < getAge(arr[i])) {
+      oldest = arr[i];
+    }
+  }
+  return oldest;
 
+  function getAge(person) {
+    if ("yearOfDeath" in person) {
+      return person.yearOfDeath - person.yearOfBirth;
+    } else {
+      return new Date().getFullYear() - person.yearOfBirth;
+    }
+  }
 };
 
 // Do not edit below this line
