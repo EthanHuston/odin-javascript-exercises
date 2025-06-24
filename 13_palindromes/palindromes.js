66b7417a63384beae3ac979a14d120de9397859b
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-24 15:45:47 -0400

    Completing challenge 13-palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+  let arr = str
+    .toLowerCase()
+    .split("")
+    .filter((val) => alphanumerical.includes(val));
+  let i = 0;
+  let j = arr.length - 1;
+  while (i < j) {
+    if (arr[i] !== arr[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
 };
 
 // Do not edit below this line
